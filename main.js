b6c93f11507e309624d4a8904eb240ff31a1a32e
Diaa-E <blarb21@yahoo.com>
2023-01-24 18:17:06 +0200

    Get geolocation of a city using city name and country code

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
 "use strict";
+
+async function getCoord(cityName, countryCode)
+{
+    //bad practice, but this is a free API key
+    const apiKey = "0a7047fb9dfbdd373f66076c81a80a4e";
+
+    try
+    {
+        const response = await fetch(
+            `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}
+            ,${countryCode}&appid=${apiKey}`,
+            {
+                mode: "cors"
+            }
+        )
+
+        return await response.json();
+    }
+    catch(err)
+    {
+        console.log(err);
+    }
+}
